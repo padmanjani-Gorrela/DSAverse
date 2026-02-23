4434be6d7bb6917f9373875db55164154c3cc3cc
Sri Padmanjani Gorrela <padmanjanigorrela55@gmail.com>
2026-02-23 18:19:34 +0530

    Update Three_Sum.java

--- Arrays/medium/Three_Sum.java
+++ Arrays/medium/Three_Sum.java
@@ -93,4 +93,36 @@ class Solution{
 
 class Solution{
   public List<List<Integer>> threeSum(int[] nums){
+    List<List<Integer>> ans = new ArrayList<>();
+    int n = nums.length;
+    Arrays.sort(nums);
+    for(int i = 0; i < n;i++){
+      if(i > 0 && nums[i] == nums[i+1]){
+        continue;
+      }
+      int j = i + 1;
+      int k = n - 1;
+      while(j <k){
+        int sum = nums[i] + nums[j] + nums[k];
+        if(sum < 0) j++;
+        else if(sum > 0) k--;
+        else{
+          List<Integer> temp = new ArrayList<>();
+          temp.add(nums[i]);
+          temp.add(nums[j]);
+          temp.add(nums[k]);
+          ans.adD(temp);
+          j++;
+          k--;
+        }
+      }
+      while(nums[j] == nums[j - 1]) j++;
+      while(nums[k] == nums[k+1]) k--;
+      return ans;
+    }
+  }
+
+  // time complexity : (nlogn) + O(n);
+  // space complexity : O(n)
+      
     
