7147a6067e4b4b086dd00a62beaec4f6b0b50ea3
Sri Padmanjani Gorrela <padmanjanigorrela55@gmail.com>
2026-02-24 19:27:33 +0530

    Add Merge Overlapping Intervals function
    Implemented a function to merge overlapping intervals based on the given problem statement.

---
+++ Arrays/Hard/Merge_overlapping_subIntervals.java
@@ -0,0 +1,11 @@
+/*
+Problem Statement: Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+Input : intervals=[[1,3],[2,6],[8,10],[15,18]]
+Output : [[1,6],[8,10],[15,18]]
+Explanation : Since intervals [1,3] and [2,6] are overlapping we can merge them to form [1,6] intervals.
+
+Input : [[1,4],[4,5]]
+Output :  [[1,5]]
+Explanation :  Since intervals [1,4] and [4,5] are overlapping we can merge them to form [1,5].
+*/
