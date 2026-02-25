e63293681ba61ce69d18293c06058df51b9a30cc
Sri Padmanjani Gorrela <padmanjanigorrela55@gmail.com>
2026-02-25 14:47:16 +0530

    Fix variable name in sum calculation

--- Arrays/Hard/Largest_Subarray_with_Sum_0.java
+++ Arrays/Hard/Largest_Subarray_with_Sum_0.java
@@ -49,7 +49,7 @@ class Solution{
     map.put(0, -1);
     int sum = 0, maxLen = 0;
     for (int i = 0; i < n; i++) {
-        sum += A[i];
+        sum += nums[i];
         if (map.containsKey(sum)) {
             maxLen = Math.max(maxLen, i - map.get(sum));
         } else {
