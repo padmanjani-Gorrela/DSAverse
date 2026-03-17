a81164c2ec0c4eb38a3007a8b13265b64cabb332
Sri Padmanjani Gorrela <padmanjanigorrela55@gmail.com>
2026-03-17 12:50:53 +0530

    Create Boats_toSavePeople.java

---
+++ Greedy/Resource Matching/Boats_toSavePeople.java
@@ -0,0 +1,31 @@
+/*
+You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+Return the minimum number of boats to carry every given person.
+
+Example 1:
+Input: people = [1,2], limit = 3
+Output: 1
+Explanation: 1 boat (1, 2)
+
+Example 2:
+Input: people = [3,2,2,1], limit = 3
+Output: 3
+Explanation: 3 boats (1, 2), (2) and (3)
+*/
+
+class Solution{
+  public int numRescueBoats(int[] people, int limit){
+    int n = people.length;
+    Arrays.sort(people);
+    int boats = 0;
+    int i = 0, j = n - 1;
+    while(i <= j){
+      if(people[i] + people[j] <= limit){
+       i++;
+      }
+      j--;
+      boats++;
+    }
+    return boats;
+  }
+}
