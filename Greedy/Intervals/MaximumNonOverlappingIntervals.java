ac8d23ccf2ad62b0c92bb75ef8019aa7b575c03e
Sri Padmanjani Gorrela <padmanjanigorrela55@gmail.com>
2026-03-17 20:55:29 +0530

    Update MaximumNonOverlappingIntervals.java

--- Greedy/Intervals/MaximumNonOverlappingIntervals.java
+++ Greedy/Intervals/MaximumNonOverlappingIntervals.java
@@ -20,7 +20,7 @@ class Solution{
     int cnt = 0;
     Arrays.sort(intervals, (a,b)- > a[1]-b[1]);
     int last = intervals[0][1];
-    for(int i = 0; i < n;i++){
+    for(int i = 1; i < n;i++){
       if(intervals[i][0] < last){
         cnt++;
       }
