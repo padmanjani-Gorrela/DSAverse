825ea2760f68a963fcbec141b1c29c6ded51814f
Sri Padmanjani Gorrela <padmanjanigorrela55@gmail.com>
2026-04-13 18:32:05 +0530

    Add brute-force solution for sliding window maximum
    Implemented a brute-force approach to find the maximum in each sliding window of size k.

--- Sliding_Window/Fixed_Window/Sliding_Window_Maximum.java
+++ Sliding_Window/Fixed_Window/Sliding_Window_Maximum.java
@@ -24,6 +24,57 @@ Front of deque = maximum of current window
 Why indices?  to remove elements leaving window.
 */
 
+Brute:
+import java.util.*;
+
+class Solution {
+    // Function to get the maximum sliding window
+    public List<Integer> maxSlidingWindow(int[] arr, int k) {
+        
+        int n = arr.length; // Size of array
+        
+        // To store the answer
+        List<Integer> ans = new ArrayList<>();
+        
+        /* Traverse on the arrary 
+        for valid window */
+        for(int i = 0; i <= n - k; i++) {
+            
+            // To store the maximum of the window
+            int maxi = arr[i];
+            
+            // Traverse the window
+            for(int j = i; j < i + k; j++) {
+                // Update the maximum
+                maxi = Math.max(maxi, arr[j]);
+            }
+            
+            // Add the maximum to the result
+            ans.add(maxi);
+        }
+        
+        // Return the stored result
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 0, -1, 3, 5, 3, 6, 8};
+        int k = 3;
+        
+        /* Creating an instance of 
+        Solution class */
+        Solution sol = new Solution();
+        
+        /* Function call to get the
+        maximum sliding window */
+        List<Integer> ans = sol.maxSlidingWindow(arr, k);
+        
+        System.out.println("The maximum elements in the sliding window are: ");
+        for(int num : ans) {
+            System.out.print(num + " ");
+        }
+    }
+}
 
 
 /*
